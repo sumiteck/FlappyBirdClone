e1608192cca7885725689b7a2c91b2be65ae74d9
sumitk68 <imsumitk1@gmail.com>
2020-02-18 23:34:58 -0500

    Give Velocity and Gravity to Sprite

--- core/src/com/sumiteck/game/FlappyBird.java
+++ core/src/com/sumiteck/game/FlappyBird.java
@@ -10,7 +10,11 @@ public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture[] bird;
+	float birdY = 0;
 	int flapstate = 0;
+	float velocity = 0;
+	int gameState = 0;
+	float gravity = 2;
 	
 	@Override
 	public void create () {
@@ -19,23 +23,45 @@ public class FlappyBird extends ApplicationAdapter {
 		bird = new Texture[2];
 		bird[0] = new Texture("bird.png");
 		bird[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2 - bird[0].getHeight()/2;
+
 
 
 	}
 
 	@Override
 	public void render () {
-		if(flapstate==0){
-			flapstate = 1;
-		}else {
-			flapstate = 0;
-		}
-		batch.begin();
-		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw((bird[flapstate]) ,Gdx.graphics.getWidth()/2  ,Gdx.graphics.getHeight()/2);
-		batch.end();
 
-	}
+
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw((bird[flapstate]), Gdx.graphics.getWidth() / 2, birdY);
+		batch.end();
+		if (gameState != 0) {
+			if (Gdx.input.justTouched()) {
+				Gdx.app.log("Screen Touched", "Yes");
+				velocity = -30;
+			}
+			if (birdY > 0 || velocity < 0) {
+				velocity = velocity + gravity;
+				birdY -= velocity;
+			}
+		}else {
+				if (Gdx.input.justTouched()) {
+					Gdx.app.log("Screen Touched", "Yes");
+					gameState = 1;
+				}
+			}
+
+			if (flapstate == 0) {
+				flapstate = 1;
+			} else {
+				flapstate = 0;
+			}
+
+
+		}
+	
 	
 	@Override
 	public void dispose () {
